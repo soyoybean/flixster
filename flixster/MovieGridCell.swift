c9eec345dcb21fbdad32fe5685bae53505a4c2c8
soyoybean <sok020@ucsd.edu>
2020-02-03 18:55:18 -0800

    Flix Collection View Setup and Layout

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Soyon Kim on 2/3/20.
+//  Copyright © 2020 Soyon Kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
