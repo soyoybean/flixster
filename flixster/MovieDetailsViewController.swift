e496bbf1444a9705dcf75509af04582bfec6a00e
soyoybean <sok020@ucsd.edu>
2020-02-03 16:33:18 -0800

    Tab Bar Controller Setup

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -7,16 +7,38 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
     
-    var movie : [String:Any]!
-
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String:Any]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropURL!)
     }
     
 
---
+++ flixster/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  flixster
+//
+//  Created by Soyon Kim on 2/3/20.
+//  Copyright © 2020 Soyon Kim. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
